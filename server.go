04d528a48af21cf3a121ec714e62e308ab62e1ff
flavio <flavio@FLAVIONOTE>
2024-06-08 14:42:22 -0300

    injecting configmap into file

--- server.go
+++ server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
 	http.ListenAndServe(":80", nil)
 }
@@ -17,4 +19,14 @@ func Hello(w http.ResponseWriter, r *http.Request) {
 	podName := os.Getenv("POD_NAME")
 
 	fmt.Fprintf(w, "<h1> Name: %s.    Age: %s.   PodeName:  %s. </h1>", name, age, podName)
+}
+
+func ConfigMap(w http.ResponseWriter, r *http.Request) {
+
+	data, err := os.ReadFile("myfamily/family.txt")
+	if err != nil {
+		log.Fatal("error reading file: ", err)
+	}
+
+	fmt.Fprintf(w, "<h1>My Family: %s </h1>", string(data))
 }
